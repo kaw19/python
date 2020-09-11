e57d5393224e25dc5680c371dc26c0d94c183559
Cláudio A. Fleury <46454054+kaw19@users.noreply.github.com>
2020-09-11 08:51:31 -0300

    Add files via upload

---
+++ Python 3 - Tópicos Básicos 1.mm
@@ -0,0 +1,503 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node BACKGROUND_COLOR="#ff6633" CREATED="1599756935943" ID="ID_917025174" MODIFIED="1599762326523" TEXT="Python 3">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node BACKGROUND_COLOR="#00cccc" CREATED="1599757476695" ID="ID_746489201" MODIFIED="1599766399445" POSITION="left" TEXT="2. Tipos de Dados">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1599757481584" ID="ID_572972911" MODIFIED="1599760351213" TEXT="int">
+<font BOLD="true" NAME="SansSerif" SIZE="13"/>
+</node>
+<node CREATED="1599757492909" ID="ID_1130702699" MODIFIED="1599760351218" TEXT="long">
+<font BOLD="true" NAME="SansSerif" SIZE="13"/>
+</node>
+<node CREATED="1599757495511" ID="ID_187284062" MODIFIED="1599760351217" TEXT="float">
+<font BOLD="true" NAME="SansSerif" SIZE="13"/>
+</node>
+<node CREATED="1599757504682" ID="ID_1182964873" MODIFIED="1599760351216" TEXT="complex">
+<font BOLD="true" NAME="SansSerif" SIZE="13"/>
+</node>
+<node CREATED="1599757507890" ID="ID_1988831723" MODIFIED="1599760351215" TEXT="bool">
+<font BOLD="true" NAME="SansSerif" SIZE="13"/>
+</node>
+<node CREATED="1599757545599" ID="ID_1693552835" MODIFIED="1599766467644">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <b>Exemplos:</b>
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1599757545599" ID="ID_1461565180" MODIFIED="1599766544867">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font color="#0033ff" face="Consolas"><b>int</b>:&#160;&#160;&#160;&#160;-786 </font>
+    </p>
+    <p>
+      <font color="#0033ff" face="Consolas"><b>long</b>:&#160;&#160;&#160;&#160;12257489574 </font>
+    </p>
+    <p>
+      <font color="#0033ff" face="Consolas"><b>float</b>:&#160;&#160;-21.9 </font>
+    </p>
+    <p>
+      <font color="#0033ff" face="Consolas"><b>complex</b>: 300 + 9.322e-36j </font>
+    </p>
+    <p>
+      <font color="#0033ff" face="Consolas"><b>bool</b>:&#160;&#160;&#160;&#160;<b>True</b>&#160;ou <b>False</b></font>
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node CREATED="1599759919656" ID="ID_858577477" MODIFIED="1599766547520" TEXT="Casting (convers&#xe3;o)">
+<font NAME="SansSerif" SIZE="12"/>
+<node CREATED="1599759947797" ID="ID_647272389" MODIFIED="1599766555945">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font face="Consolas" color="#0033ff"><b>type(var)</b></font>
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1599759968825" ID="ID_532445365" MODIFIED="1599759998234">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <b><font color="#0033ff" face="Consolas">c = 2.1 </font></b>
+    </p>
+    <p>
+      <b><font color="#0033ff" face="Consolas">print(type(c))</font></b>
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node CREATED="1599759959016" FOLDED="true" ID="ID_1873660794" MODIFIED="1599766792402">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <b>int(), float(), str(), bool(), complex().</b>
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1599760055982" ID="ID_1853782768" MODIFIED="1599760152890">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font face="Consolas" color="#0033ff">int('2553')</font>
+    </p>
+  </body>
+</html>
+</richcontent>
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1599760066651" ID="ID_183040344" MODIFIED="1599760133623">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font color="#0033ff" face="Consolas">str(2553.45)</font>
+    </p>
+  </body>
+</html>
+</richcontent>
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+</node>
+</node>
+<node BACKGROUND_COLOR="#ff99cc" CREATED="1599760281244" ID="ID_1994503440" MODIFIED="1599766609356" POSITION="right" TEXT="1. Conc.s B&#xe1;sicos">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node BACKGROUND_COLOR="#66ff66" CREATED="1599756957015" ID="ID_1748658774" MODIFIED="1599762328470" TEXT="E/S">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1599756991704" ID="ID_714539801" MODIFIED="1599762328472" TEXT="B&#xe1;sica">
+<node CREATED="1599756998032" ID="ID_838082752" MODIFIED="1599764634714" TEXT="print()">
+<node CREATED="1599757122317" ID="ID_1210272753" MODIFIED="1599757153367" TEXT="mostra informa&#xe7;&#xe3;o na interface do interpretador"/>
+<node CREATED="1599757255320" ID="ID_1529868536" MODIFIED="1599764646283" TEXT="Sa&#xed;da b&#xe1;sica">
+<node CREATED="1599757315283" ID="ID_1663604869" MODIFIED="1599757360299">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font face="Consolas" color="#0033ff"><b>print</b></font><font face="Consolas">(&quot;\n*** Dados do Funcion&#225;rio ***&quot;)&#160;</font>
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+<node CREATED="1599757263363" ID="ID_248140782" MODIFIED="1599764663867" TEXT="Sa&#xed;da formatada">
+<node CREATED="1599757155217" ID="ID_1907202827" MODIFIED="1599757305102">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font face="Consolas" color="#0033ff"><b>print</b></font><font face="Consolas">(&quot;%-25s %7s&#160;&#160;&#160;%-20s&quot; % (&quot;Nome&quot;,&quot;Sal&#225;rio R$&quot;,&quot;Empresa&quot;))</font>
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+</node>
+<node CREATED="1599757004328" ID="ID_1253492374" MODIFIED="1599764724145" TEXT="input()">
+<node CREATED="1599757010436" ID="ID_705374206" MODIFIED="1599757046277" TEXT="retorna valor digitado como string"/>
+<node CREATED="1599757057073" ID="ID_752495883" MODIFIED="1599757442005">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font face="Consolas">slr = </font><font face="Consolas" color="#0033ff"><b>float</b></font><font face="Consolas">(</font><font face="Consolas" color="#0033ff"><b>input</b></font><font face="Consolas">(&quot;Entre o sal&#225;rio:&#160;&#160;R$ &quot;))</font>
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+</node>
+</node>
+<node BACKGROUND_COLOR="#ffff33" CREATED="1599757975741" ID="ID_572284073" MODIFIED="1599762328482" TEXT="Vari&#xe1;veis">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1599757688470" ID="ID_1742766452" MODIFIED="1599765444598" TEXT="Identificadores">
+<node CREATED="1599757718371" ID="ID_1968272638" MODIFIED="1599765451000" TEXT="regras">
+<node CREATED="1599757761025" ID="ID_58856871" MODIFIED="1599757802991" TEXT="combina&#xe7;&#xe3;o de caracteres &apos;a-z&apos; e &apos;A-Z&apos;, algarismos &apos;0-9&apos; e sublinhado &apos;_&apos;"/>
+<node CREATED="1599757693660" ID="ID_1328908327" MODIFIED="1599757717773" TEXT="n&#xe3;o pode ser palavras-chave"/>
+<node CREATED="1599757831966" ID="ID_319661250" MODIFIED="1599757834572" TEXT="n&#xe3;o pode come&#xe7;ar com algarismo"/>
+<node CREATED="1599757934148" ID="ID_1410515602" MODIFIED="1599757950077" TEXT="pode ter qualquer comprimento"/>
+</node>
+</node>
+<node CREATED="1599757990350" ID="ID_1483823497" MODIFIED="1599765405627" TEXT="Comando Atribui&#xe7;&#xe3;o">
+<node CREATED="1599757996565" ID="ID_1352855963" MODIFIED="1599758007982" TEXT="&lt;id_vari&#xe1;vel&gt; [operador] &lt;cte/express&#xe3;o&gt;"/>
+<node CREATED="1599758014474" ID="ID_689341215" MODIFIED="1599758077873">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Exemplo:&#160;&#160;<font face="Consolas" color="#0033ff"><b>xl = ML + 10</b></font>
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1599758261090" ID="ID_393617997" MODIFIED="1599758298382">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Atribui&#231;&#227;o M&#250;ltipla: <font face="Consolas" color="#0033ff"><b>b, c, d = 2, 9, a</b></font>
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+</node>
+<node CREATED="1599758309594" ID="ID_1536079234" MODIFIED="1599762328490" TEXT="Express&#xf5;es">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1599765975586" ID="ID_1697263283" MODIFIED="1599765980783" TEXT="Constantes"/>
+<node CREATED="1599758315289" FOLDED="true" ID="ID_258953004" MODIFIED="1599766778214" TEXT="Operadores">
+<node CREATED="1599758320074" ID="ID_201594735" MODIFIED="1599765999255" TEXT="Aritm&#xe9;ticos">
+<node CREATED="1599758327047" ID="ID_50178984" MODIFIED="1599758705317">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font face="Consolas" color="#0033ff"><b>+ - * /&#160;&#160;% ** //</b></font>
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+<node CREATED="1599758386590" ID="ID_303331139" MODIFIED="1599766033027" TEXT="Relacionais">
+<node CREATED="1599758395582" ID="ID_461246787" MODIFIED="1599758632074">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <b><font color="#0033ff" face="Consolas">&gt;, &lt;, &gt;=, &lt;=, &lt;&gt;, !=, ==</font></b>
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+<node CREATED="1599758425539" ID="ID_433326082" MODIFIED="1599766057888" TEXT="L&#xf3;gicos">
+<node CREATED="1599758430740" ID="ID_865519844" MODIFIED="1599766066756">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font color="#0033ff" face="Consolas"><b>and,&#160;or&#160;</b></font>&#160;e<font color="#0033ff" face="Consolas"><b>&#160;&#160;not</b></font>
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node CREATED="1599758450943" ID="ID_1666112088" MODIFIED="1599766071283" TEXT="Bit a bit">
+<node CREATED="1599758462841" ID="ID_973778" MODIFIED="1599766111934" TEXT="&amp; (e), | (ou), ^ (ou excl.), ~ (compl.1), &lt;&lt; &gt;&gt; (desloc.s)"/>
+</node>
+<node CREATED="1599758529188" ID="ID_13053476" MODIFIED="1599766117554" TEXT="Pertin&#xea;ncia">
+<node CREATED="1599758545063" ID="ID_858350838" MODIFIED="1599758593325">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <b><font color="#0033ff" face="Consolas">in</font></b>&#160;&#160;e&#160;&#160;<b><font color="#0033ff" face="Consolas">not in</font></b>
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+<node CREATED="1599758753063" ID="ID_1413521667" MODIFIED="1599766143501" TEXT="Identidade">
+<node CREATED="1599758545063" ID="ID_528455367" MODIFIED="1599758793288">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <b><font color="#0033ff" face="Consolas">is</font></b>&#160;&#160;e&#160;&#160;<b><font color="#0033ff" face="Consolas">is not</font></b>
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+</node>
+<node CREATED="1599758822956" FOLDED="true" ID="ID_374012487" MODIFIED="1599766776968" TEXT="Fun&#xe7;&#xf5;es">
+<node CREATED="1599759339216" ID="ID_1070880802" MODIFIED="1599766299151" TEXT="Nativas">
+<node CREATED="1599759747494" ID="ID_1697618819" MODIFIED="1599766372217" TEXT="abs(), delattr(), hash(), memoryview(), set(), all(), dict(), help(), min(), setattr(), any(), dir(), hex(), next(), slice(), ascii(), divmod(), id(), object(), sorted(), bin(), enumerate(), input(), oct(), staticmethod(), bool(), eval(), int(), open(), str(), breakpoint(), exec(), isinstance(), ord(), sum(), bytearray(), filter(), issubclass(), pow(), super(), bytes(), float(), iter(), print(), tuple(), callable(), format(), len(), property(), type(), chr(), frozenset(), list(), range(), vars(), classmethod(), getattr(), locals(), repr(), zip(), compile(), globals(), map(), reversed(), import(), complex(), hasattr(), max(), round()"/>
+</node>
+<node CREATED="1599758837196" ID="ID_1982493447" MODIFIED="1599766250693">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      M&#243;d. <font color="#0033ff"><b>math</b></font>
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1599758841622" ID="ID_516877458" MODIFIED="1599758932042">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p style="margin-bottom: 0cm; line-height: 100%">
+      abs(x), ceil(x), cmp(x, y), exp(x), fabs(x), floor(x), log(x), log10(x)
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1599759115591" ID="ID_1608613854" MODIFIED="1599759172214" TEXT="acos(x), asin(x), atan(x), atan2(y, x), cos(x), hypot(x, y), sin(x), tan(x), degrees(x), radians(x)"/>
+</node>
+<node CREATED="1599759270326" ID="ID_646728143" MODIFIED="1599766295173">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      M&#243;d. <b><font color="#0033ff">random</font></b>
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1599759278471" ID="ID_880495344" MODIFIED="1599759320940" TEXT="choice(seq), randrange([start,] stop [,step]), random(), seed([x]), shuffle(lst)"/>
+</node>
+</node>
+</node>
+</node>
+<node BACKGROUND_COLOR="#ffcccc" CREATED="1599760504536" ID="ID_1979371546" MODIFIED="1599766795266" POSITION="left" TEXT="3. String">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1599760559016" ID="ID_135971922" MODIFIED="1599761527729">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <ul>
+      <li>
+        Sequ&#234;ncia de caracteres
+      </li>
+      <li>
+        Tipo de dados derivado
+      </li>
+      <li>
+        Arranjos de bytes
+      </li>
+    </ul>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1599760857188" ID="ID_1466490165" MODIFIED="1599766812277" TEXT="Delimitadores">
+<node CREATED="1599760871576" ID="ID_1220803444" MODIFIED="1599760884751" TEXT="aspas simples ou duplas">
+<node CREATED="1599766820511" ID="ID_507537767" MODIFIED="1599766843836" TEXT="&apos;ok&apos;, &quot;ela disse: &apos;Eu n&#xe3;o!&apos;&quot;"/>
+</node>
+</node>
+<node CREATED="1599760669671" ID="ID_1347758664" MODIFIED="1599766865746" TEXT="Exemplos">
+<node CREATED="1599760673775" ID="ID_387305669" MODIFIED="1599760798648">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font color="#0033ff" face="Consolas"><b>print(&quot;Ol&#225; Pessoal!&quot;) </b></font>
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1599760673775" ID="ID_1448800027" MODIFIED="1599760769455">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font color="#0033ff" face="Consolas"><b>a = &quot;&quot;&quot;IFG </b></font>
+    </p>
+    <p>
+      <font color="#0033ff" face="Consolas"><b>Curso de Python </b></font>
+    </p>
+    <p>
+      <font color="#0033ff" face="Consolas"><b>Prof. Cl&#225;udio&quot;&quot;&quot; </b></font>
+    </p>
+    <p>
+      <font color="#0033ff" face="Consolas"><b>print(a)</b></font>
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+<node CREATED="1599761005759" ID="ID_173605738" MODIFIED="1599766927870" TEXT="comprimento">
+<node CREATED="1599761016111" ID="ID_1947763247" MODIFIED="1599761040845">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font color="#0033ff" face="Consolas">len()</font>
+    </p>
+  </body>
+</html></richcontent>
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+<node CREATED="1599761005759" ID="ID_1155431961" MODIFIED="1599766962030" TEXT="slicing">
+<font ITALIC="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1599761016111" ID="ID_1210615530" MODIFIED="1599761193554">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font color="#0033ff" face="Consolas">s = &quot;IFG - Inst. Fed. de Goi&#225;s&quot; </font>
+    </p>
+    <p>
+      <font color="#0033ff" face="Consolas">print(s[0:3]) </font>
+    </p>
+    <p>
+      <font color="#009933" face="Consolas">IFG</font>
+    </p>
+  </body>
+</html></richcontent>
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+<node CREATED="1599761250048" ID="ID_595907023" MODIFIED="1599761266089" TEXT="n&#xe3;o admite atribui&#xe7;&#xe3;o de item">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1599761294774" ID="ID_142702735" MODIFIED="1599766989451" TEXT="concatena&#xe7;&#xe3;o">
+<node CREATED="1599761016111" ID="ID_1334129048" MODIFIED="1599761478578">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font color="#0033ff" face="Consolas">s1 = &quot;IFG&quot; </font>
+    </p>
+    <p>
+      <font color="#0033ff" face="Consolas">s2 = &quot; - Inst. Fed. de Goi&#225;s&quot; </font>
+    </p>
+    <p>
+      <font color="#0033ff" face="Consolas">print(s1 + s2) </font>
+    </p>
+    <p>
+      <font color="#009933" face="Consolas">IFG - Inst. Fed. de Goi&#225;s</font>
+    </p>
+  </body>
+</html></richcontent>
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+<node CREATED="1599761294774" ID="ID_775449272" MODIFIED="1599767011702" TEXT="replica&#xe7;&#xe3;o">
+<node CREATED="1599761016111" ID="ID_208202175" MODIFIED="1599761349268">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font color="#0033ff" face="Consolas">s = &quot;IFG - Inst. Fed. de Goi&#225;s&quot; </font>
+    </p>
+    <p>
+      <font color="#0033ff" face="Consolas">print(s[0:3] * 4) </font>
+    </p>
+    <p>
+      <font color="#009933" face="Consolas">IFGIFGIFGIFG</font>
+    </p>
+  </body>
+</html></richcontent>
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+<node CREATED="1599761572459" ID="ID_225878847" MODIFIED="1599767051970" TEXT="m&#xe9;todos">
+<node CREATED="1599761578794" ID="ID_352092387" MODIFIED="1599762187671" TEXT="upper(), lower(), split(), replace()"/>
+</node>
+</node>
+</node>
+</map>
